f64101a2992d1acbc83478ec89369b2de63920bb
Hank Tom <hanktom@gmail.com>
2021-11-09 11:40:05 +0800

    Star line 1

---
+++ src/main/java/StarLine.java
@@ -0,0 +1,14 @@
+public class StarLine {
+    public static void main(String[] args) {
+        for (int i=0; i<10; i++) {
+            for (int j = 0; j < 20; j++) {
+                if (i*2==j) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
