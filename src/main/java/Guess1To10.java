4ebae16c4a84cc81bef0038c3f4085896757a9ce
Hank Tom <hanktom@gmail.com>
2021-11-09 10:27:44 +0800

    initial commit

---
+++ src/main/java/Guess1To10.java
@@ -0,0 +1,11 @@
+public class Guess1To10 {
+    public static void main(String[] args) {
+
+        for (int i=0; i<3; i++) {
+            //使用者猜數字
+            //判斷
+            System.out.println("Hello");
+        }
+
+    }
+}
---
+++ src/main/java/Stars.java
@@ -0,0 +1,11 @@
+public class Stars {
+    public static void main(String[] args) {
+
+        for (int i=0;i <10; i++) {
+
+            System.out.print("*");
+        }
+
+
+    }
+}
