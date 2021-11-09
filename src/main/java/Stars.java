a95c3260e157365ad64c9859b48f8b8de8e8ce51
Hank Tom <hanktom@gmail.com>
2021-11-09 11:28:38 +0800

    star border v1

--- src/main/java/Stars.java
+++ src/main/java/Stars.java
@@ -1,8 +1,12 @@
 public class Stars {
     public static void main(String[] args) {
-        for (int j=0; j<10; j++) {
-            for (int i = 0; i < 20; i++) {
-                System.out.print("*");
+        for (int i=0; i<10; i++) {
+            for (int j = 0; j < 20; j++) {
+                if (j==0 || j==19 || (j>=2 && j<=17)) {
+                    System.out.print("*");
+                }else {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
