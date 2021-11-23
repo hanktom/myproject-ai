ad0c42456c9f63b6a188aef397cddf26be0746c4
Hank Tom <hanktom@gmail.com>
2021-11-23 11:36:38 +0800

    VendingMachine p3

--- src/main/java/com/tom/UsingSwitch.java
+++ src/main/java/com/tom/UsingSwitch.java
@@ -1,22 +1,39 @@
 package com.tom;
 
+import java.util.Scanner;
+
 public class UsingSwitch {
     public static void main(String[] args) {
-        int n = 1;
-        switch (n) {
-            case 1:
-                System.out.println("1");
-                break;
-            case 2:
-                System.out.println("2");
-                break;
-            case 3:
-                System.out.println("3");
-                break;
-            default:
-                System.out.println("other");
-                break;
+        int total = 0;
+        boolean exit = false;
+        Scanner scanner = new Scanner(System.in);
+        while(!exit) {
+            System.out.println("Please put your coin(1/5/10):");
+            String s = scanner.next();
+            try {
+                int coin = Integer.parseInt(s);
+                switch (coin) {
+                    case 1:
+                    case 5:
+                    case 10:
+                        total = total + coin;
+                        break;
+                    case 0:
+                        System.out.println("Returning your coins...");
+                        exit = true;
+                        break;
+                    default:
+                        System.out.println("other");
+                        break;
+                }
+                System.out.println("total:" + total);
+            } catch (NumberFormatException e) {
+                System.out.println("Exp!!");
+            }
+
+
+
         }
-        System.out.println("end");
+
     }
 }
