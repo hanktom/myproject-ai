88913b29fcb4284ea995526104ecb2ced4b48da9
Hank Tom <hanktom@gmail.com>
2021-12-07 10:45:48 +0800

    rolling dice and counting

---
+++ src/main/java/com/tom/ArrayTester.java
@@ -0,0 +1,13 @@
+package com.tom;
+
+public class ArrayTester {
+    public static void main(String[] args) {
+        float[] weights = new float[8];
+        weights[0] = 66.5f;
+        System.out.println(weights[1]);
+        boolean end = false;
+        boolean[] aa = new boolean[5];
+        aa[0] = true;
+        System.out.println(aa[1]);
+    }
+}
---
+++ src/main/java/com/tom/DiceCounter.java
@@ -0,0 +1,27 @@
+package com.tom;
+
+import java.util.Random;
+
+public class DiceCounter {
+    //rolling dice 10 times
+    //counting points
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] counters = new int[6];
+        for (int i = 0; i < 10; i++) {
+            int n = random.nextInt(6)+1;
+            System.out.println(n);
+            counters[n-1]++;
+        }
+        int max = 0;
+        int point = 0;
+        for (int i = 0; i < 6; i++) {
+            //System.out.println((i+1) + " " + counters[i] + " times");
+            if (counters[i] > max) {
+                max = counters[i];
+                point = i;
+            }
+        }
+        System.out.println((point+1) + " " + max + " times");
+    }
+}
