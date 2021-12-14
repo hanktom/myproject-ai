f9ba2a4c3fed93e7816d6674e38ae402fa88c32f
Hank Tom <hanktom@gmail.com>
2021-12-14 10:26:48 +0800

    using String to show poker types

--- src/main/java/com/tom/Poker.java
+++ src/main/java/com/tom/Poker.java
@@ -2,10 +2,11 @@ package com.tom;
 
 public class Poker {
     public static void main(String[] args) {
+        String tt = "cdhs";
         char[] types = {'c', 'd', 'h', 's'};
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 13; j++) {
-                System.out.print((j+1)+""+types[i]);
+                System.out.print((j+1)+""+ tt.charAt(i));
                 System.out.print(" ");
             }
             System.out.println();
--- src/main/java/com/tom/Tester.java
+++ src/main/java/com/tom/Tester.java
@@ -2,6 +2,9 @@ package com.tom;
 
 public class Tester {
     public static void main(String[] args) {
+        String s = "abcdefg";
+        System.out.println(s.length());
+        System.out.println(s.charAt(7));
         int a = 3;
         int b = 5;
         int tmp = a;
